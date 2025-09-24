1f24953ecc670a4a4f7ae4c10f80a190512098c2
Ankur Mallick <ankurmallick80@gmail.com>
2025-09-24 17:20:44 +0530

    small update

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 
 function Dashboard() {
   const [active, setActive] = useState(0);
@@ -11,13 +10,11 @@ function Dashboard() {
   
   return (
     <div className="d-flex flex-row h-100">
-      <ul className="list-group list-group-flush border-end border-3">
-        <li className="list-group-item">
-          <div className="d-flex flex-row justify-content-between gap-5">
-            <p className="fs-3 fw-semibold">My Tickets</p>
-            <i className="bi bi-filter fs-2"></i>
-          </div>
-        </li>
+      <ul className="list-group list-group-flush border-end border-3" style={{width: '300px'}}>
+        <div className="d-flex flex-row list-group-item justify-content-between gap-5">
+          <p className="fs-3 fw-semibold m-0">My Tickets</p>
+          <i className="bi bi-filter fs-2"></i>
+        </div>
         {tickets.map((ticket, index) => 
           <TicketLink 
             key={index} 
@@ -25,21 +22,22 @@ function Dashboard() {
             text={ticket.text}>
           </TicketLink>)}
       </ul>
-      <div className="p-3">
-        <p>YEAH</p>
+      <div className="d-flex">
+        <p className="fs-3 fw-semibold p-2">First Ticket</p>
       </div>
     </div>
   )
 
-  function TicketLink({index, text}) {
-    return(
-      <Link 
-        to="#" 
-        className={`list-group-item list-group-item-action ${index===active && "active"}`}
-        onClick={() => setActive((active+1) % tickets.length)}>
-          {text}
-      </Link>
-    )
+  function TicketLink({index, text}) { 
+    return( 
+      <button 
+        type="button"
+        className={`list-group-item list-group-item-action ${index===active && "active"}`} 
+        style={{wordBreak: 'break-word'}}
+        onClick={() => setActive((active+1) % tickets.length)}> 
+          {text} 
+      </button> 
+    ) 
   }
 }
 
